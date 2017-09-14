f1e9fb751c306768d5a1d6a715e4c1e9b8b619c8
GearShark123 <lylespurrell@hotmail.com>
2017-09-14 11:32:00 -0400

    [ADD] ButtonControl Script, Spawn Gameobject, and Clone of Green Sprite

---
+++ COMP305-F2017/Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonControl : MonoBehaviour
+{
+    public Transform spawn;
+    public GameObject sprite;
+
+    void OnMouseDown()
+    {     
+        Destroy(GameObject.Find("Lyle Self Portrait (Green)(Clone)"));
+        Destroy(GameObject.Find("Lyle Self Portrait (Blue)(Clone)"));
+        Destroy(GameObject.Find("Lyle Self Portrait (Red)(Clone)"));
+        Instantiate(sprite, spawn.transform.position, spawn.transform.rotation);            
+    }
+}
