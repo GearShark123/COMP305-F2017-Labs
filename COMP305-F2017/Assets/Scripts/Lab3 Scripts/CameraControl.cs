c0a2739d95bd8193e92c0ceabe03a71b878f5bc4
GearShark123 <lylespurrell@hotmail.com>
2017-09-28 11:13:22 -0400

    [MOD] 3 Areas Working

--- COMP305-F2017/Assets/Scripts/Lab3 Scripts/CameraControl.cs
+++ COMP305-F2017/Assets/Scripts/Lab3 Scripts/CameraControl.cs
@@ -2,27 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraControl : MonoBehaviour {
+public class CameraControl : MonoBehaviour
+{
 
     public Transform player;
     public Transform area2;
+    public Transform area3;
     public Camera cam;
     public int area;
+    public float track;
 
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
     void Area(int num)
     {
         area = num;
+
+        if(num == 3)
+        {
+            track = player.position.x;
+        }        
     }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
 
-        if(area == 1)
+        if (area == 1)
         {
             Follow();
         }
@@ -30,16 +40,31 @@ public class CameraControl : MonoBehaviour {
         {
             ZoomOut();
         }
+        else
+        {            
+            ZoomIn();
+        }
     }
 
     void Follow()
     {
         this.transform.position = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+        cam.orthographicSize = 4;
     }
 
     void ZoomOut()
     {
         this.transform.position = new Vector3(area2.position.x, area2.position.y, this.transform.position.z);
         cam.orthographicSize = 7;
+    }
+
+    void ZoomIn()
+    {        
+        this.transform.position = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+        if (Input.GetKey(KeyCode.D) && player.position.x < area3.position.x+6.2f && cam.orthographicSize >= 1.2f && player.position.x >= track)
+        {
+            cam.orthographicSize -= 0.027f;
+            track = player.position.x;
+        }
     }
 }
