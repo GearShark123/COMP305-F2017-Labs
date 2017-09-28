3bd6f1b24b750170317c1df061b5512124337036
GearShark123 <lylespurrell@hotmail.com>
2017-09-27 21:22:31 -0400

    [ADD] Area 1 and Area 2 Working

--- COMP305-F2017/Assets/Scripts/Lab3 Scripts/CameraControl.cs
+++ COMP305-F2017/Assets/Scripts/Lab3 Scripts/CameraControl.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
+    public Transform player;
+    public Transform area2;
+    public Camera cam;
+    public int area;
+
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
+    void Area(int num)
+    {
+        area = num;
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if(area == 1)
+        {
+            Follow();
+        }
+        else if (area == 2)
+        {
+            ZoomOut();
+        }
+    }
 
     void Follow()
     {
+        this.transform.position = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+    }
 
+    void ZoomOut()
+    {
+        this.transform.position = new Vector3(area2.position.x, area2.position.y, this.transform.position.z);
+        cam.orthographicSize = 7;
     }
 }
---
+++ COMP305-F2017/Assets/Scripts/Lab3 Scripts/PlayerArea.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArea : MonoBehaviour {
+
+    public Camera cam;
+    public int num;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {        
+            cam.SendMessage("Area", num);        
+    }
+}
