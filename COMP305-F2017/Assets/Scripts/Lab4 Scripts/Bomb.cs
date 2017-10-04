8bf5a204e5215a02a1dc201e228e2e9b4f27cd10
GearShark123 <lylespurrell@hotmail.com>
2017-10-04 12:13:06 -0400

    [ADD] Explosion Script

---
+++ COMP305-F2017/Assets/Scripts/Lab4 Scripts/Bomb.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+    private int counter;
+
+    // Use this for initialization
+    void Start () {
+        counter = 0;
+        Invoke("MakeExplosion",3f);
+	}	
+
+    void MakeExplosion()
+    {
+        // Create Explosion 
+    }
+}
---
+++ COMP305-F2017/Assets/Scripts/Lab4 Scripts/Explosion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    public float explosionRadius = 50.0f;
+    public float explosionForce = 1.0f;
+
+    private Rigidbody rBody;
+    private RaycastHit2D[] hits;
+
+    // Use this for initialization
+    void Start()
+    {
+        rBody = this.GetComponent<Rigidbody>();
+
+        hits = Physics2D.CircleCastAll(this.transform.position, explosionRadius, Vector2.zero);
+
+        foreach (RaycastHit2D h in hits)
+        {
+            if (h.transform.gameObject.tag == "Robot")
+                h.rigidbody.AddForce((h.transform.position - this.transform.position) * explosionForce, ForceMode2D.Impulse);
+        }
+    }
+}
