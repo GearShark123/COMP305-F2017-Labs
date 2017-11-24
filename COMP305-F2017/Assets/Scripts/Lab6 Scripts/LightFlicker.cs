20fec6e13b78bf9f5e134488242f646e51dbced8
Unknown <lylespurrell@hotmail.com>
2017-11-24 12:45:56 -0500

    [ADD] Light Flicker

---
+++ COMP305-F2017/Assets/Scripts/Lab6 Scripts/LightFlicker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LightFlicker : MonoBehaviour
+{
+    public Light light;
+    private System.Random random = new System.Random();
+    private bool isLess = false;
+    public float maxValue;  // 15
+    public float minValue;  // 12
+    public float change;    // 0.1
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (light.intensity + change <= maxValue && isLess == true)
+        {
+            light.intensity += change; 
+            if (light.intensity == maxValue)
+            {
+                isLess = false;
+            }
+        }
+        else
+        {
+            light.intensity -= change;
+            if (light.intensity <= minValue)
+            {
+                isLess = true;
+            }
+        }
+    }
+}
